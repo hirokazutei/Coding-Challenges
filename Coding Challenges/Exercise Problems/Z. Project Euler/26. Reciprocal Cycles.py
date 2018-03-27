b0ad4afde3c5e0c98272cdf3a4244e0846eba7bd
Hirokazu <hirokazu.tei@gmail.com>
2018-03-27 12:53:16 +0900

    Forgot that I haven't updated the test code.

--- Coding Challenges/Exercise Problems/Z. Project Euler/26. Reciprocal Cycles.py
+++ Coding Challenges/Exercise Problems/Z. Project Euler/26. Reciprocal Cycles.py
@@ -17,9 +17,38 @@ Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be see
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 """
 
-for i in range(2, 1000):
-    a = str(1/i)
-    if len(a) > 10:
-        if a[-2] != a[-3]:
-            if a[-2] != a[-4]:
-                print(a)
+# Brainstorm
+"""
+Think back to when we used to do division by hand. We try to fit the divisor into the remainder, if the divisor is bigger
+than the remainder, we add a zero to the end. The subtraction would give us an even division or another remainder.
+Theoretically, if the remainder is a number we have encountered before, it will repeat a cycle.
+Thus, we can solve this problem by dividing the numbers just like by hand and keeping track of the remainders that
+we have encountered.
+"""
+
+def ReciprocalCyclesA(num):
+    array = [0, 0]
+    for i in range(2, num):
+        already_divided = []
+        count = 0
+        remainder = 10
+        while True:
+            if remainder % i == 0:
+                break
+            elif remainder // i == 0:
+                remainder *= 10
+                continue
+            else:
+                remainder = remainder - i * (remainder // i)
+                if remainder in already_divided:
+                    break
+                already_divided.append(remainder)
+                count += 1
+        array.append(count)
+    largest = max(array)
+    for i in range(len(array)):
+        if array[i] == largest:
+            return i
+
+print(ReciprocalCyclesA(1000))
+
